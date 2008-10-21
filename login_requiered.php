74a97d14f0564d75292fca32224a2968d48337a3
shakabut <shakabut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-21 12:22:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/login-required/trunk@69745 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ login_requiered.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Login requiered WordPress Plugin
+Plugin URI: 
+Based on : http://blog.taragana.com/index.php/archive/angsumans-authenticated-wordpress-plugin-password-protection-for-your-wordpress-blog/
+Description: This plugin allows you to make your WordPress site accessible to logged in users only. In other words to view your site they have to create / have an account in your site and be logged in. No configuration necessary. Simply activating the plugin is all that is required from you.
+
+Author: Stefan Nicolae
+Version: 0.1
+Author URI: http://shakabut.com/
+License: Free to use non-commercially.
+Warranties: None.
+*/
+//print_r($user);
+function ac_auth_redirect() {
+	// Checks if a user is logged in, if not redirects them to the login page
+	//echo session_id();
+	//print_r($_COOKIE);
+	//echo COOKIEHASH."|".PASS_COOKIE."|".AUTH_COOKIE;
+	
+	if (!is_user_logged_in())
+	{
+		nocache_headers();
+		header("HTTP/1.1 302 Moved Temporarily");
+		header('Location: ' . get_settings('siteurl') . '/wp-login.php?redirect_to=' . urlencode($_SERVER['REQUEST_URI']));
+		header("Status: 302 Moved Temporarily");
+		exit();
+	}
+	else {
+		//echo "you are registered!";
+	};
+}
+
+if('wp-login.php' != $pagenow && 'wp-register.php' != $pagenow) add_action('template_redirect', 'ac_auth_redirect');
+?>
---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Plugin Name ===
+Contributors: shakabut
+Tags: login required, authentication
+Requires at least: 2.6.2
+Tested up to: 2.6.2
+Stable tag: 0.1
+
+Permit access to your blog only to the logged in users
+
+== Description ==
+
+This plugin enables you to permit access to your blog only to the logged in users.
+
+== Installation ==
+1. Upload `login_requiered.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
